bb565bb27cc3f751ff4214a2c626f38900e66ced
Ted Weber <thweber@ebay.com>
2020-03-24 14:31:15 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rl_agents
+
+A place to experiment with various RL algorithms while working through _Reinforcement Learning: An Introduction_, by Richard Sutton and Andrew Barto.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+INSTALL_REQUIREMENTS = [
+      "numpy==1.16.3",
+      "tensorflow==1.13.1",
+      "gym==0.17.1",
+      "jupyter==1.0.0"
+]
+
+setup(name="rl_algorithms",
+      python_requires=">3.5",
+      packages=find_packages(exclude=("tests", "tests.*")),
+      include_package_data=True,
+      description="RL algorithms for various tasks",
+      author="Theodore Weber",
+      author_email="weber.ted2@gmail.com",
+      install_requires=INSTALL_REQUIREMENTS,
+      zip_safe=False)
+
